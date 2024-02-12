86185047901103bceba98a20f699e9f3e4a19bc4
Max Hopf <maxhopf@pm.me>
2024-02-12 12:01:27 +0100

    Add function to play a game
    function will call getComputerChoice() and getPlayerChoice() and compare
    the two values. It will return a string that communicates that the
    player either has won or lost the game or that it's a tie.

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -16,3 +16,22 @@ function getPlayerChoice() {
     } else return null;
 }
 
+function playGame(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+    if (playerSelection === computerSelection) {
+        return `It's a tie!`
+    } else if (playerSelection === `rock` && computerSelection === `paper`) {
+        return `You Lose! Paper beats Rock`;
+    } else if (playerSelection === `rock` && computerSelection === `scissors`) {
+        return `You Win! Rock beats Scissors`;
+    } else if (playerSelection === `paper` && computerSelection === `rock`) {
+        return `You Win! Paper beats Rock`;
+    } else if (playerSelection === `paper` && computerSelection === `scissors`) {
+        return `You Lose! Scissors beats Paper`
+    } else if (playerSelection === `scissors` && computerSelection === `rock`) {
+        retrun`You Lose! Rock beats Scissors`
+    } else if (playerSelection === `scissors` && computerSelection === `paper`) {
+        retrun`You Win! Scissory beats Paper`
+    }
+}
