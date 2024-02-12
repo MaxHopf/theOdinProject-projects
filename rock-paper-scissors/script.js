24847001a6c8722e096846a9b20c804671ac6a1d
Max Hopf <maxhopf@pm.me>
2024-02-12 13:55:47 +0100

    Add playGame() and two global variables for scoring
    * playGame() added to allow multiple rounds of the game
    * trace scoring with two global variables computerScore and playerScore

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,11 @@
 "use strict"
 
 console.log("Rock the Game");
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
+
     const computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice == 0) return `rock`;
     else if (computerChoice == 1) return `paper`;
@@ -10,6 +13,7 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
+
     let userInput = prompt(`Your choice: "Rock", "Paper" or "Scissors"`).toLowerCase();
     if (userInput === `rock` || userInput === `paper` || userInput === `scissors`) {
         return userInput;
@@ -19,17 +23,38 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === computerSelection) {
-        return `It's a tie!`
+        console.log(`It's a tie!`);
     }
     else if ((playerSelection === `rock` && computerSelection === `paper`) ||
         (playerSelection === `paper` && computerSelection === `scissors`) ||
         (playerSelection === `scissors` && computerSelection === `rock`)) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
     }
     else if ((playerSelection === `rock` && computerSelection === `scissors`) ||
         (playerSelection === `paper` && computerSelection === `rock`) ||
         (playerSelection === `scissors` && computerSelection === `paper`)) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
+
+function playGame() {
+
+    playerScore = 0;
+    computerScore = 0;
+
+    for (let i = 0; i <= 5; i++) {
+        playRound(getPlayerChoice(), getComputerChoice());
+        console.log(playerScore, computerScore);
+    }
+
+    if (playerScore > computerScore) {
+        return `Hurray! You win the Game!`;
+    } else if (playerScore < computerScore) {
+        return `Buuuh! You lose the Game!`;
+    } else {
+        return `It's a tie!`
     }
 }
 
