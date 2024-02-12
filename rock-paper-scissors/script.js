53fa6f2bac2a7c2a150470eeaf346e16354bde9a
Max Hopf <maxhopf@pm.me>
2024-02-12 14:10:36 +0100

    Improve computerChoice()
    * change if statement to switch case

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -7,9 +7,16 @@ let computerScore = 0;
 function getComputerChoice() {
 
     const computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+        case 0: return `rock`;
+        case 1: return `paper`;
+        case 2: return `scissors`;
+        default: return `Error: Something is wrong`;
+    }
+    /*
     if (computerChoice == 0) return `rock`;
     else if (computerChoice == 1) return `paper`;
-    else return `scissors`;
+    else return `scissors`;*/
 }
 
 function getPlayerChoice() {
