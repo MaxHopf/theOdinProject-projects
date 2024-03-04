76920a806a58582cb97e20f6abdfe9a3e9d862fb
Max Hopf <maxhopf@pm.me>
2024-03-04 19:14:57 +0100

    Add basic math functions and initial html

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const display = document.querySelector('#display');
+
+function add(x, y) {
+    let z = null;
+    return z = x + y;
+}
+
+function substract(x, y) {
+    let z = null;
+    return z = x - y;
+}
+
+function multiply(x, y) {
+    let z = null;
+    return z = x * y;
+}
+
+function divide(x, y) {
+    let z = null;
+    return z = x / y;
+}
+
+function operate(oper, num1, num2) {
+    if (oper == 'add') {
+        add(num1, num2);
+    } else if (oper == 'substract') {
+        substract(num1, num2);
+    } else if (oper == 'multiply') {
+        multiply(num1, num2);
+    } else if (oper == 'divide') {
+        divide(num1, num2);
+    }
+}
